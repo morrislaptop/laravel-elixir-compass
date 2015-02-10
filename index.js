b3d1ed917f5f460455d279e9f22b63daf3e4fbd3
Craig Morris <craig.michael.morris@gmail.com>
2015-02-10 01:06:52 +0000

    First version

---
+++ index.js
@@ -0,0 +1,22 @@
+var elixir = require('laravel-elixir');
+var compile = require('./ingredients/commands/CompileCSS');
+var _ = require('underscore');
+
+elixir.extend('compass', function (src, output, options) {
+
+	options = _.extend({
+		config_file: './config.rb',
+		sass: './resources/assets/compass',
+		css: './public/css'
+	}, options);
+
+	return compile({
+		compiler: 'Compass',
+		pluginName: 'compass',
+		pluginOptions: options,
+		src: src,
+		output: output,
+		search: '**/*.+(sass|scss)'
+	});
+
+});
