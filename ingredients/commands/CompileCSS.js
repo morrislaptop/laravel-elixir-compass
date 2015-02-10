b14100511237337d066e0d8e09f3ac2992d60593
Craig Morris <craig.michael.morris@gmail.com>
2015-02-10 01:26:20 +0000

    Some fixes

--- ingredients/commands/CompileCSS.js
+++ ingredients/commands/CompileCSS.js
@@ -1,5 +1,16 @@
+/**
+ * Copied from https://github.com/laravel/elixir/blob/master/ingredients/commands/CompileCSS.js
+ * 
+ * We can not include this file directly, as the plugins variable below does not include 
+ * the gulp-compass plugin.
+ * 
+ * Also for some reason gulp-load-plugins can't find gulp-compass so we are loading and
+ * using that explicitly. 
+ * 
+ */ 
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
+var compass = require('gulp-compass');
 var config = require('laravel-elixir').config;
 var utilities = require('laravel-elixir/ingredients/commands/Utilities');
 var Notification = require('laravel-elixir/ingredients/commands/Notification');
@@ -18,7 +29,7 @@ module.exports = function(options) {
 
     gulp.task(options.pluginName, function() {
         return gulp.src(src)
-            .pipe(plugins[options.pluginName](options.pluginOptions)).on('error', onError)
+            .pipe(compass(options.pluginOptions)).on('error', onError)
             .pipe(plugins.autoprefixer())
             .pipe(plugins.if(config.production, plugins.minifyCss()))
             .pipe(gulp.dest(options.output || config.cssOutput))
