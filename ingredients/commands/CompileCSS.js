4203785a73d03b1796fecbf61cd1f2b05f91143f
Craig Morris <craig.michael.morris@gmail.com>
2015-02-10 01:50:20 +0000

    Revert to plugins

--- ingredients/commands/CompileCSS.js
+++ ingredients/commands/CompileCSS.js
@@ -10,7 +10,6 @@
  */ 
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
-var compass = require('gulp-compass');
 var config = require('laravel-elixir').config;
 var utilities = require('laravel-elixir/ingredients/commands/Utilities');
 var Notification = require('laravel-elixir/ingredients/commands/Notification');
@@ -29,7 +28,7 @@ module.exports = function(options) {
 
     gulp.task(options.pluginName, function() {
         return gulp.src(src)
-            .pipe(compass(options.pluginOptions)).on('error', onError)
+            .pipe(plugins[options.pluginName](options.pluginOptions)).on('error', onError)
             .pipe(plugins.autoprefixer())
             .pipe(plugins.if(config.production, plugins.minifyCss()))
             .pipe(gulp.dest(options.output || config.cssOutput))
